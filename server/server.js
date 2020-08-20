0f7b247f28e7dc948ad7bee5e62b34ca0b5d3885
Nagarjuna DNV <dnvnagarjunaaa@gmail.com>
2020-08-20 19:23:40 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const path=require('path');
 const express= require('express');
 const app=express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port= process.env.PORT;
+const publicPath = path.join(__dirname, '..', 'build');
+const port= process.env.PORT || 4000;
 
 app.use(express.static(publicPath));
 
-app.get('*',(req,res)=>{
+app.get('/*',(req,res)=>{
     res.sendFile(path.join(publicPath,'index.html'));
 })
 app.listen(port,()=>{
-    console.log("server is running on",port);
+    console.log("server is running on port:",port);
 })
